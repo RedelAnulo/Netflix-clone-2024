a56c9a0cd928e202eb1dd228e94e4c9ed9adbbeb
Redela Anulo <84re65de01la60@gmail.com>
2024-05-03 09:43:51 +0300

    Header and Footer

---
+++ netflix-clone/src/pages/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../../Componts/Header/Header'
+import Footer from '../../Componts/Footer/Footer'
+
+function Home() {
+  return (
+    <>
+      <Header/>
+      <Footer/>
+    </>
+  )
+}
+
+export default Home
