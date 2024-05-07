e91885d6e2d078e3e0bb38d5da8a5bfd45b77a6e
Redela Anulo <84re65de01la60@gmail.com>
2024-05-07 10:37:55 +0300

    Banner

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -2,9 +2,11 @@ import "./App.css";
 import Home from "./pages/Home/Home";
 
 function App() {
-  return <div className="App">
-    <Home/>
-  </div>;
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
 }
 
 export default App;
--- netflix-clone/src/pages/Home/Home.js
+++ netflix-clone/src/pages/Home/Home.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from '../../Componts/Header/Header'
 import Footer from '../../Componts/Footer/Footer'
+import Banner from '../../Componts/Banner/Banner'
 
 function Home() {
   return (
     <>
       <Header/>
+      <Banner/>
       <Footer/>
     </>
   )
