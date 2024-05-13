888cb9848493194d27d8a6fb2fab7f2f4b0cfffe
Redela Anulo <84re65de01la60@gmail.com>
2024-05-13 21:43:31 +0300

    Row

--- netflix-clone/src/pages/Home/Home.js
+++ netflix-clone/src/pages/Home/Home.js
@@ -2,12 +2,14 @@ import React from 'react'
 import Header from '../../Componts/Header/Header'
 import Footer from '../../Componts/Footer/Footer'
 import Banner from '../../Componts/Banner/Banner'
+import RowList from '../../Componts/Rows/RowList/RowList'
 
 function Home() {
   return (
     <>
       <Header/>
       <Banner/>
+      <RowList/>
       <Footer/>
     </>
   )
